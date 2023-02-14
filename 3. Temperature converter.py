517838e96d3c579e45fd4584c6c90b662707c39d
Tanvir <122251878+Tanvir-Ahasan@users.noreply.github.com>
2023-02-14 14:13:22 +0600

    Update 3. Temperature converter.py

--- 3. Temperature converter.py
+++ 3. Temperature converter.py
@@ -1,10 +1,16 @@
 temperature = float(input("Enter temperature: "))
-unit = input("Enter unit (C or F): ")
+unit = input("Enter unit (C, F, or K): ").upper()
 if unit == 'C':
-  fahrenheit = (temperature * 9/5) + 32
-  print(f"{temperature}°C is equal to {fahrenheit}°F.")
+    fahrenheit = (temperature * 9/5) + 32
+    kelvin = temperature + 273.15
+    print(f"{temperature}°C is equal to {fahrenheit}°F and {kelvin}K.")
 elif unit == 'F':
-  celsius = (temperature - 32) * 5/9
-  print(f"{temperature}°F is equal to {celsius}°C.")
+    celsius = (temperature - 32) * 5/9
+    kelvin = (temperature + 459.67) * 5/9
+    print(f"{temperature}°F is equal to {celsius}°C and {kelvin}K.")
+elif unit == 'K':
+    celsius = temperature - 273.15
+    fahrenheit = (temperature * 9/5) - 459.67
+    print(f"{temperature}K is equal to {celsius}°C and {fahrenheit}°F.")
 else:
-  print("Invalid unit.")
+    print("Invalid unit.")
